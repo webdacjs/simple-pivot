563ca1b76c78eea46cb3a4e450af3adb49ba8160
Juan Convers <me@jco.im>
2020-12-27 14:36:58 +0100

    Adapting test to new dataset.

--- __tests__/index.test.ts
+++ __tests__/index.test.ts
@@ -1,125 +1,127 @@
+// import { readJsonConfigFile } from 'typescript'
 import simplePivot from '../src/index'
-import iris from './iris.json'
+import players from './players.json'
 
 test('Testing sum', () => {
-    expect.assertions(3)
-    const res = simplePivot(iris, { groupField: 'species', valueField: 'petal_width', pivotFunction: 'sum' })
-    expect(res[0].species).toBe('setosa')
-    expect(parseInt(res[0].petal_width)).toBe(12)
+    expect.assertions(5)
+    const res = simplePivot(players, { groupField: 'gender', valueField: 'goals', pivotFunction: 'sum' })
+    expect(res[0].gender).toBe('male')
+    expect(parseInt(res[0].goals)).toBe(15)
+    expect(res[1].gender).toBe('female')
+    expect(parseInt(res[1].goals)).toBe(22)
     expect(res[0].pivotFunction).toBe('sum')
 })
 
 test('Testing count', () => {
     expect.assertions(3)
-    const res = simplePivot(iris, { groupField: 'species', valueField: 'petal_width', pivotFunction: 'count' })
-    expect(res[0].species).toBe('setosa')
-    expect(res[0].petal_width).toBe(50)
+    const res = simplePivot(players, { groupField: 'colour', valueField: 'name', pivotFunction: 'count' })
+    expect(res[0].colour).toBe('green')
+    expect(res[0].name).toBe(3)
     expect(res[0].pivotFunction).toBe('count')
 })
 
 test('Testing counta', () => {
     expect.assertions(3)
-    const res = simplePivot(iris, { groupField: 'species', valueField: 'petal_width', pivotFunction: 'counta' })
-    expect(res[0].species).toBe('setosa')
-    expect(res[0].petal_width).toBe(50)
+    const res = simplePivot(players, { groupField: 'country', valueField: 'name', pivotFunction: 'counta' })
+    expect(res[3].country).toBe('Spain')
+    expect(res[3].name).toBe(2)
     expect(res[0].pivotFunction).toBe('counta')
 })
 
 test('Testing average (mean)', () => {
     expect.assertions(3)
-    const res = simplePivot(iris, { groupField: 'species', valueField: 'petal_width', pivotFunction: 'mean' })
-    expect(res[0].species).toBe('setosa')
-    expect(res[0].petal_width).toBe(0.2439999999999999)
+    const res = simplePivot(players, { groupField: 'gender', valueField: 'age', pivotFunction: 'mean' })
+    expect(res[0].gender).toBe('male')
+    expect(res[0].age).toBe(25.333333333333332)
     expect(res[0].pivotFunction).toBe('mean')
 })
 
 test('Testing average (avg)', () => {
     expect.assertions(3)
-    const res = simplePivot(iris, { groupField: 'species', valueField: 'petal_width', pivotFunction: 'avg' })
-    expect(res[0].species).toBe('setosa')
-    expect(res[0].petal_width).toBe(0.2439999999999999)
+    const res = simplePivot(players, { groupField: 'gender', valueField: 'age', pivotFunction: 'avg' })
+    expect(res[0].gender).toBe('male')
+    expect(res[0].age).toBe(25.333333333333332)
     expect(res[0].pivotFunction).toBe('avg')
 })
 
 test('Testing average (average)', () => {
     expect.assertions(3)
-    const res = simplePivot(iris, { groupField: 'species', valueField: 'petal_width', pivotFunction: 'average' })
-    expect(res[0].species).toBe('setosa')
-    expect(res[0].petal_width).toBe(0.2439999999999999)
+    const res = simplePivot(players, { groupField: 'gender', valueField: 'age', pivotFunction: 'average' })
+    expect(res[0].gender).toBe('male')
+    expect(res[0].age).toBe(25.333333333333332)
     expect(res[0].pivotFunction).toBe('average')
 })
 
 test('Testing median', () => {
     expect.assertions(3)
-    const res = simplePivot(iris, { groupField: 'species', valueField: 'petal_width', pivotFunction: 'median' })
-    expect(res[0].species).toBe('setosa')
-    expect(res[0].petal_width).toBe(0.2)
+    const res = simplePivot(players, { groupField: 'gender', valueField: 'age', pivotFunction: 'median' })
+    expect(res[0].gender).toBe('male')
+    expect(res[0].age).toBe(24)
     expect(res[0].pivotFunction).toBe('median')
 })
 
 test('Testing mode', () => {
     expect.assertions(3)
-    const res = simplePivot(iris, { groupField: 'species', valueField: 'petal_width', pivotFunction: 'mode' })
-    expect(res[0].species).toBe('setosa')
-    expect(res[0].petal_width).toBe(0.2)
+    const res = simplePivot(players, { groupField: 'gender', valueField: 'age', pivotFunction: 'mode' })
+    expect(res[0].gender).toBe('male')
+    expect(res[0].age).toBe(23)
     expect(res[0].pivotFunction).toBe('mode')
 })
 
 test('Testing min', () => {
     expect.assertions(3)
-    const res = simplePivot(iris, { groupField: 'species', valueField: 'petal_width', pivotFunction: 'min' })
-    expect(res[0].species).toBe('setosa')
-    expect(res[0].petal_width).toBe(0.1)
-    expect(res[0].pivotFunction).toBe('min')
+    const res = simplePivot(players, { groupField: 'gender', valueField: 'age', pivotFunction: 'min' })
+    expect(res[1].gender).toBe('female')
+    expect(res[1].age).toBe(19)
+    expect(res[1].pivotFunction).toBe('min')
 })
 
 test('Testing max', () => {
     expect.assertions(3)
-    const res = simplePivot(iris, { groupField: 'species', valueField: 'petal_width', pivotFunction: 'max' })
-    expect(res[0].species).toBe('setosa')
-    expect(res[0].petal_width).toBe(0.6)
+    const res = simplePivot(players, { groupField: 'gender', valueField: 'age', pivotFunction: 'max' })
+    expect(res[0].gender).toBe('male')
+    expect(res[0].age).toBe(32)
     expect(res[0].pivotFunction).toBe('max')
 })
 
 test('Testing no pivot function (just grouping)', () => {
     expect.assertions(3)
-    const res = simplePivot(iris, { groupField: 'species', valueField: 'petal_width' })
-    expect(res[0].species).toBe('setosa')
-    expect(res[0].petal_width.length).toBe(50)
+    const res = simplePivot(players, { groupField: 'country', valueField: 'gender' })
+    expect(res[0].country).toBe('USA')
+    expect(res[0].gender.length).toBe(1)
     expect(res[0].pivotFunction).toBe(undefined)
 })
 
-test('Testing sum with valueField array', () => {
-    expect.assertions(3)
-    const res = simplePivot(iris, { groupField: 'species', valueField: ['petal_width'], pivotFunction: 'sum' })
-    expect(res[0].species).toBe('setosa')
-    expect(parseInt(res[0].petal_width)).toBe(12)
+test('Testing sum with value Array', () => {
+    expect.assertions(5)
+    const res = simplePivot(players, { groupField: 'gender', valueField: ['goals'], pivotFunction: 'sum' })
+    expect(res[0].gender).toBe('male')
+    expect(parseInt(res[0].goals)).toBe(15)
+    expect(res[1].gender).toBe('female')
+    expect(parseInt(res[1].goals)).toBe(22)
     expect(res[0].pivotFunction).toBe('sum')
 })
 
 test('Testing sum with multiple valueField array', () => {
-    expect.assertions(4)
-    const res = simplePivot(iris, {
-        groupField: 'species',
-        valueField: ['petal_width', 'petal_length'],
+    expect.assertions(8)
+    const res = simplePivot(players, {
+        groupField: 'gender',
+        valueField: ['shots', 'goals'],
         pivotFunction: 'sum',
     })
-    expect(res[0].species).toBe('setosa')
-    expect(parseInt(res[0].petal_width)).toBe(12)
-    expect(parseInt(res[0].petal_length)).toBe(73)
+    expect(res[0].gender).toBe('male')
+    expect(parseInt(res[0].shots)).toBe(44)
+    expect(parseInt(res[0].goals)).toBe(15)
     expect(res[0].pivotFunction).toBe('sum')
+    expect(res[1].gender).toBe('female')
+    expect(parseInt(res[1].shots)).toBe(29)
+    expect(parseInt(res[1].goals)).toBe(22)
+    expect(res[1].pivotFunction).toBe('sum')
 })
 
 // Errors
 test('Testing to group for a non existing field', () => {
     expect(() => {
-        simplePivot(iris, { groupField: 'non-existing', valueField: ['petal_width'], pivotFunction: 'sum' })
+        simplePivot(players, { groupField: 'non-existing', valueField: ['petal_width'], pivotFunction: 'sum' })
     }).toThrow()
 })
-
-test('Testing to omit value field', () => {
-    expect(() => {
-        simplePivot(iris, { groupField: 'species'})
-    }).toThrow()
-})
-
