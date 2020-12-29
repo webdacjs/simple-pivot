aebeafe502ee33b457ec13ca0086c0700069444f
Juan Convers <me@jco.im>
2020-12-29 05:54:02 +0100

    Fixing typo.

--- src/validate.ts
+++ src/validate.ts
@@ -1,6 +1,6 @@
 export default function validate (groupValues: Array<string>, pivotconfig: PivotConfig) {
     if (!groupValues) {
-        throw Error(`There is no group values ij the data`)
+        throw Error(`There is no group values in the data`)
     }
     if (groupValues.length === 1 && groupValues[0] === undefined) {
         throw Error(`The groupField ${pivotconfig.groupField} does not exist in the data`)
